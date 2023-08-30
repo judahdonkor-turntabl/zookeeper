a16f47b4a79bf54bbb020b4f4cd7920f7477b74c
judahdonkor <judahdonkor@gmail.com>
2023-08-30 11:25:40 +0000

    Initial Commit For Token Processing Module

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/oidc/AccessTokenProcessor.java
@@ -0,0 +1,11 @@
+package org.apache.zookeeper.server.auth.oidc;
+
+/**
+ * <h2>Main Class For Converting Access Token String To The Standard Structure</h2>
+ */
+public class AccessTokenProcessor {
+
+  public AccessToken process(byte[] authData) {
+    throw new UnsupportedOperationException();
+  }
+}
