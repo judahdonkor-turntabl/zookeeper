e3ed973ea73cd08008c3f4f719be0769de94d598
vizard-xi <kelvin.prince.336@gmail.com>
2023-08-31 13:16:11 +0000

    Implement and Configure Matches OIDC Authentication Provider Method

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/OIDCAuthenticationProvider.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/OIDCAuthenticationProvider.java
@@ -26,7 +26,7 @@ public class OIDCAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public boolean matches(String id, String aclExpr) {
-        return false;
+        return id.matches(aclExpr);
     }
 
     @Override
