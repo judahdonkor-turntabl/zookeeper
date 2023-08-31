d8d95e1e1af3029565c3ee3f60dcd0b2440a9b90
vizard-xi <kelvin.prince.336@gmail.com>
2023-08-31 10:37:47 +0000

    Configured handleAuthentication function in OIDCAuthentticationProvider class to get user and group IDs and add them to the server connection authInfo

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/OIDCAuthenticationProvider.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/OIDCAuthenticationProvider.java
@@ -3,25 +3,39 @@ package org.apache.zookeeper.server.auth;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.server.ServerCnxn;
+import org.apache.zookeeper.server.auth.oidc.AccessToken;
+import org.apache.zookeeper.server.auth.oidc.AccessTokenProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OIDCAuthenticationProvider implements AuthenticationProvider {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OIDCAuthenticationProvider.class);
+
     @Override
     public String getScheme() {
-        return null;
+        return "oidc";
     }
 
     @Override
     public KeeperException.Code handleAuthentication(ServerCnxn cnxn, byte[] authData) {
-        return null;
+        List<Id> ids = handleAuthentication(authData);
+
+        if (!ids.isEmpty()) {
+            ids.forEach(cnxn::addAuthInfo);
+            return KeeperException.Code.OK;
+        }
+        return KeeperException.Code.AUTHFAILED;
     }
 
     @Override
     public List<Id> handleAuthentication(HttpServletRequest request, byte[] authData) {
-        return AuthenticationProvider.super.handleAuthentication(request, authData);
+        return handleAuthentication(authData);
     }
 
     @Override
@@ -42,5 +56,25 @@ public class OIDCAuthenticationProvider implements AuthenticationProvider {
     @Override
     public String getUserName(String id) {
         return AuthenticationProvider.super.getUserName(id);
+    }
+
+    private List<Id> handleAuthentication(final byte[] authData) {
+
+        final List<Id> ids = new ArrayList<>();
+
+        try {
+            AccessToken accessToken = new AccessTokenProcessor().process(authData);
+            if (!accessToken.getGroups().isEmpty()) {
+                accessToken.getGroups().forEach(groupID -> ids.add(new Id(getScheme(), "gid:" + groupID)));
+            }
+
+            if (!accessToken.getUserID().isEmpty()) {
+                ids.add(new Id(getScheme(), "uid:" + accessToken.getUserID()));
+            }
+        } catch (Exception exception) {
+            LOG.error(exception.getMessage());
+        }
+
+        return Collections.unmodifiableList(ids);
     }
 }
