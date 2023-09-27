12328c18815eb7c0f64a245ffa6afdcc44ff52fc
Vincent Adjetey Adjei <vincent.adjei@turntabl.io>
2023-09-27 11:00:40 +0000

    construct uri with headers and parameters

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParser.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParser.java
@@ -1,5 +1,14 @@
 package org.apache.zookeeper.server.auth.oidc;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
 public class TokenIntrospectionParser implements AccessTokenParser {
 
     private final AccessTokenParameterType accessTokenParameterType;
@@ -44,15 +53,55 @@ public class TokenIntrospectionParser implements AccessTokenParser {
 
     @Override
     public AccessToken parse(byte[] accessTokenBytes) {
+
+        HttpClient httpClient = HttpClients.createDefault();
+        String url = constructURL(accessTokenBytes);
+        HttpResponse response;
+
+        try {
+            HttpUriRequest uriRequest = buildUri(url);
+            response = httpClient.execute(uriRequest);
+        } catch (ClientProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Do something with response
+
         throw new UnsupportedOperationException();
     }
 
+    public HttpUriRequest buildUri(String url){
+
+        RequestBuilder requestBuilder;
+
+        switch (method){
+            case GET:
+                requestBuilder = RequestBuilder.get(url)
+                        .addHeader("get", "getter")
+                        .addParameter("abc", "def");
+                break;
+            case POST:
+                requestBuilder = RequestBuilder.post(url)
+                        .addHeader("post", "poster")
+                        .addParameter("mno", "qrt");
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+
+        return requestBuilder.build();
+    }
+
     public enum Method {
-        GET
+        GET,
+        POST
     }
 
     public enum AccessTokenParameterType {
-        QUERY_PARAMETER, PATH_PARAMETER,
+        QUERY_PARAMETER,
+        PATH_PARAMETER
     }
 
     @Override
--- zookeeper-server/src/test/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParserTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParserTest.java
@@ -1,11 +1,13 @@
 package org.apache.zookeeper.server.auth.oidc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.zookeeper.server.auth.oidc.TokenIntrospectionParser.AccessTokenParameterType;
 import org.apache.zookeeper.server.auth.oidc.TokenIntrospectionParser.Method;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TokenIntrospectionParserTest {
 
@@ -49,5 +51,22 @@ class TokenIntrospectionParserTest {
         String url = tokenIntrospectionParser.constructURL("example_access_token".getBytes());
 
         assertNotEquals("https://auth-server.com/tokeninfo?accessToken=example_access_token",url);
+    }
+
+    @Test
+    void constructURI() {
+        TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
+                AccessTokenParameterType.QUERY_PARAMETER,
+                "access_token",
+                "https://auth-server.com/tokeninfo",
+                Method.GET);
+
+        HttpUriRequest uriRequest = tokenIntrospectionParser.buildUri(
+                tokenIntrospectionParser.constructURL("example_access_token".getBytes())
+        );
+
+        assertNotNull(uriRequest);
+        assertEquals(1, uriRequest.getAllHeaders().length);
+        assertEquals("getter", uriRequest.getFirstHeader("get").getValue());
     }
 }
