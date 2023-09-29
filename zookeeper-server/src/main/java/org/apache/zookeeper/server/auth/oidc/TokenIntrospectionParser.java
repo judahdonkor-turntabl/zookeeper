87a55ed9d502fc60713b03318e881e2e7c7cf23a
vizard-xi <kelvin.prince.336@gmail.com>
2023-09-29 05:30:56 +0000

    Dynamically add access token to URL

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParser.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParser.java
@@ -5,6 +5,9 @@ public class TokenIntrospectionParser implements AccessTokenParser {
     private final AccessTokenParameterType accessTokenParameterType;
     private final String accessTokenParameterName;
     private final String tokenIntrospectionEndpoint;
+
+    private final String accessToken = "{accessToken}";
+
     private final Method method;
 
     public TokenIntrospectionParser(AccessTokenParameterType accessTokenParameterType,
@@ -15,9 +18,18 @@ public class TokenIntrospectionParser implements AccessTokenParser {
         this.method = method;
     }
 
-    public String constructURL(byte[] accessTokenBytes) {
-        if(accessTokenParameterType.equals(AccessTokenParameterType.PATH_PARAMETER)){
-            return tokenIntrospectionEndpoint + "/" + accessTokenParameterName + "/" + new String(accessTokenBytes);
+    public String constructURL(byte[] accessTokenBytes) throws Exception {
+        if(accessTokenParameterType.equals(AccessTokenParameterType.PATH_PARAMETER)) {
+            if (accessTokenParameterName.contains(accessToken))
+            {
+                return String.format("%s/%s", tokenIntrospectionEndpoint, accessTokenParameterName)
+                        .replace(accessToken, new String(accessTokenBytes));
+            }
+
+            throw new Exception(accessToken.concat(" is not present in the URL provided."));
+        }
+        else if(accessTokenParameterType.equals(AccessTokenParameterType.QUERY_PARAMETER)){
+            return tokenIntrospectionEndpoint + "?" + accessTokenParameterName + "=" + new String(accessTokenBytes);
         }
         return null;
     }
@@ -27,6 +39,8 @@ public class TokenIntrospectionParser implements AccessTokenParser {
 //no standard regarding endpoint
 
 //    https://auth-server.com/tokeninfo?access_token={accessToken}
+
+//    https://auth-server.com/tokeninfo/access_token/{accessToken}
 
 //    https://auth-server.com/tokeninfo?accessToken={accessToken}
 
--- zookeeper-server/src/test/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParserTest.java
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/auth/oidc/TokenIntrospectionParserTest.java
@@ -1,6 +1,9 @@
 package org.apache.zookeeper.server.auth.oidc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.zookeeper.server.auth.oidc.TokenIntrospectionParser.AccessTokenParameterType;
 import org.apache.zookeeper.server.auth.oidc.TokenIntrospectionParser.Method;
@@ -12,17 +15,86 @@ class TokenIntrospectionParserTest {
      * Rename Test Cases Appropriately
      */
     @Test
-    void constructURLUsingPathParameterTest() {
+    public void testURLConstructionUsingPathParam() throws Exception {
+
+        String tokenIntrospectionEndpoint = "https://auth-server.com/tokeninfo";
+        String accessTokenParameterName = "access_token/{accessToken}";
+        byte[] accessToken = "testing".getBytes();
+
+
         TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
                 AccessTokenParameterType.PATH_PARAMETER,
+                accessTokenParameterName, tokenIntrospectionEndpoint, Method.GET);
+
+        String actualURL = tokenIntrospectionParser.constructURL("testing".getBytes());
+        String expectedURL = tokenIntrospectionEndpoint + "/" + accessTokenParameterName.replace("{accessToken}", new String(accessToken));
+
+        assertEquals(expectedURL, actualURL);
+    }
+
+    @Test
+    public void testURLConstructionUsingPathParamWithExceptionThrown() {
+
+        String tokenIntrospectionEndpoint = "https://auth-server.com/tokeninfo";
+        String accessTokenParameterName = "access_token/";
+        byte[] accessToken = "testing".getBytes();
+
+
+        TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
+                AccessTokenParameterType.PATH_PARAMETER,
+                accessTokenParameterName, tokenIntrospectionEndpoint, Method.GET);
+
+        Exception exception = assertThrows(Exception.class, () -> {
+            tokenIntrospectionParser.constructURL(accessToken);
+        });
+
+        String expectedMessage = "{accessToken} is not present in the URL provided.";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void testURLConstructionUsingInvalidPathParam() throws Exception {
+
+        String tokenIntrospectionEndpoint = "https://auth-server.com/tokeninfo";
+        String accessTokenParameterName = "accessToken";
+        byte[] accessToken = "testing".getBytes();
+
+
+        TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
+                AccessTokenParameterType.PATH_PARAMETER,
+                accessTokenParameterName, tokenIntrospectionEndpoint, Method.GET);
+
+        String actualURL = tokenIntrospectionParser.constructURL("testing".getBytes());
+        String expectedURL = tokenIntrospectionEndpoint + "/" + "access_token/{accessToken}".replace("{accessToken}", new String(accessToken));
+
+        assertNotEquals(expectedURL, actualURL);
+    }
+
+    @Test
+    void constructURLUsingQueryParameterWithValidAccessTokenParameterName() throws Exception {
+        TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
+                AccessTokenParameterType.QUERY_PARAMETER,
                 "access_token",
                 "https://auth-server.com/tokeninfo",
                 Method.GET);
 
-        String url = tokenIntrospectionParser.constructURL("ya29.c.b0Aaekm1KrZxCpPeHv1cTV803Z7jawVmrrlphlIiaSVBmYXjhH0vWRMYorQmsvGw08RZwm6aUUbzLWEKL4LglgHVBxQaSrm68lAVEWa6O1jkfRDwbMFgZVmo79dN6Y6DX3PXYO5vHaVSG4woD7zJhyDvKIqio8otrdXJ5XosbDbm5Cq1V0QhOR4x994rwE_mf3DN5TM-2YqEa07CojitsPjNglU_C1UhdOrnwIpac5z1iMRjQUZ_EG3TtcbjnfhoTiAhAPPJxXwbzZxJqjkajzeBNukwUbU0GuOhFsguN22qzS68aAJqYqx9ZrxCBtUdzG-ncC2l_zY2wG340DBvvhqiioFrjZ8rjthMdgfrz9tzWW7MXQzpI8t00c46QWpQRIJntgg5QMxxv3ysyS_flv63tvbYBe_jXOO5Ixbx0VZXpbOIYOiaOb3Yh7U3R04eB8SM_mR55bg_JIqFxVeh0ofghVfWqIs-078SU09_ie_31b7XbesbaS_XIUdoRte99Z2iq79cS8eg3BcZ-xftFm8Woai6pz09hU58Iev56o70oiviJxw5sW-rv_FuiyuXxo578BB3m9oB7fmS7qQYn1B8o0p9hqOSqgfmxtXhJOJtYrpz4-V-0Y48qgiUmdvB8-XskI81Jep2t2wR02uuS8O3OJOSxJzj0vYVWlv2nU7-J5YQZM350jVm-M3tgtm166w1a_SB4muIWbVw05IUZb4izVjUXO64moRsjrfFQ437qpijSJxg6pvBvmdVisoc6iFvtqU_sVWXbfVVQhzlkq6F4pI25VYqV5Xc-Zrp7buu7Vd95jufVOZ4-2gU8vcU5cRBBXQeQshSkf1ZW-MYt2ogWk1fjv74k9Ur-a4m3luFIuFfpUpibnee7ZOyOeWt-ogZx8IJt23bF7cOwra4mIsXZOF59qYeQlj9q24J3pqZhZvqZ8UstkjrFv4gr7r6lb1a2sY05p358Ui048e-3ak2sruR3vFjXdSnxn_WUjzhz2prljzqVqjbb"
-                .getBytes());
+        String url = tokenIntrospectionParser.constructURL("example_access_token".getBytes());
 
-        assertEquals("https://auth-server.com/tokeninfo/access_token/ya29.c.b0Aaekm1KrZxCpPeHv1cTV803Z7jawVmrrlphlIiaSVBmYXjhH0vWRMYorQmsvGw08RZwm6aUUbzLWEKL4LglgHVBxQaSrm68lAVEWa6O1jkfRDwbMFgZVmo79dN6Y6DX3PXYO5vHaVSG4woD7zJhyDvKIqio8otrdXJ5XosbDbm5Cq1V0QhOR4x994rwE_mf3DN5TM-2YqEa07CojitsPjNglU_C1UhdOrnwIpac5z1iMRjQUZ_EG3TtcbjnfhoTiAhAPPJxXwbzZxJqjkajzeBNukwUbU0GuOhFsguN22qzS68aAJqYqx9ZrxCBtUdzG-ncC2l_zY2wG340DBvvhqiioFrjZ8rjthMdgfrz9tzWW7MXQzpI8t00c46QWpQRIJntgg5QMxxv3ysyS_flv63tvbYBe_jXOO5Ixbx0VZXpbOIYOiaOb3Yh7U3R04eB8SM_mR55bg_JIqFxVeh0ofghVfWqIs-078SU09_ie_31b7XbesbaS_XIUdoRte99Z2iq79cS8eg3BcZ-xftFm8Woai6pz09hU58Iev56o70oiviJxw5sW-rv_FuiyuXxo578BB3m9oB7fmS7qQYn1B8o0p9hqOSqgfmxtXhJOJtYrpz4-V-0Y48qgiUmdvB8-XskI81Jep2t2wR02uuS8O3OJOSxJzj0vYVWlv2nU7-J5YQZM350jVm-M3tgtm166w1a_SB4muIWbVw05IUZb4izVjUXO64moRsjrfFQ437qpijSJxg6pvBvmdVisoc6iFvtqU_sVWXbfVVQhzlkq6F4pI25VYqV5Xc-Zrp7buu7Vd95jufVOZ4-2gU8vcU5cRBBXQeQshSkf1ZW-MYt2ogWk1fjv74k9Ur-a4m3luFIuFfpUpibnee7ZOyOeWt-ogZx8IJt23bF7cOwra4mIsXZOF59qYeQlj9q24J3pqZhZvqZ8UstkjrFv4gr7r6lb1a2sY05p358Ui048e-3ak2sruR3vFjXdSnxn_WUjzhz2prljzqVqjbb",
-                url);
+        assertEquals("https://auth-server.com/tokeninfo?access_token=example_access_token",url);
+    }
+
+    @Test
+    void constructURLUsingQueryParameterWithInvalidAccessTokenParameterName() throws Exception {
+        TokenIntrospectionParser tokenIntrospectionParser = new TokenIntrospectionParser(
+                AccessTokenParameterType.QUERY_PARAMETER,
+                "access_token",
+                "https://auth-server.com/tokeninfo",
+                Method.GET);
+
+        String url = tokenIntrospectionParser.constructURL("example_access_token".getBytes());
+
+        assertNotEquals("https://auth-server.com/tokeninfo?accessToken=example_access_token",url);
     }
 }
